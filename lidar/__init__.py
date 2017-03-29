12e90c76c4db93043ef9946d897161068483ea46
Giles Dring <gilesdring@gmail.com>
2017-03-29 23:01:59 +0100

    Working version of the Lidar class

--- lidar/__init__.py
+++ lidar/__init__.py
@@ -1,59 +1,77 @@
+'''Module to house the Lidar class'''
 import os
 import re
 import numpy
+import png
 
 
-class Lidar:
+def coords(filename):
+    '''Calculate the coordinates of a filename'''
+    return (int(filename[2:4]), int(filename[4:6]))
+
+
+class Lidar(object):
     '''Class to handle reading and processing LIDAR data'''
-    def _make_range(self, base):
-        return [str(x + int(base)) for x in range(10)]
 
     def __init__(self, model, resolution, base):
+        self.dirname = None
+        self.start_cell = None
+        self.grid_size = None
+        self.matrix = None
         self.model = model
-        self.resolution = resolution
+        dimension = re.findall(r'(\d+)(\D+)', resolution)[0]
+        self.cell_size = int(1e6 / (
+            int(dimension[0]) * (1000 if dimension[1] == 'm' else 1)))
         self.base = base
-        eastings = self._make_range(base[2:4])
-        northings = self._make_range(base[4:6])
-        self.cells = [(x+y) for y in northings for x in eastings]
 
-        # TODO make this work with resolution of CM, not just M
-        self.dimension = 1000/int(re.findall(r'\d+', self.resolution)[0])
-        self.result = numpy.empty((self.dimension*10, self.dimension*10))
-        self.result.fill(numpy.nan)
-
-    def _process_cell(self, cell, index):
-        def fname(cell):
-            return ''.join([self.base[0:2], cell, '_', self.model, '_', self.resolution, '.asc'])
-        filename = fname(cell)
-        return filename, os.path.exists(filename)
+    def _process_cell(self, filename):
+        location = [c-self.start_cell[i]
+                    for i, c
+                    in enumerate(coords(filename))]
+        # print x, y, dimension, path
+        x_start = location[0] * self.cell_size
+        y_start = (self.grid_size[1]-1-location[1]) * self.cell_size
+        print y_start
+        try:
+            data = numpy.loadtxt(os.path.join(self.dirname, filename),
+                                 skiprows=6)
+            data[data == -9999] = numpy.nan
+            self.matrix[y_start:y_start+self.cell_size,
+                        x_start:x_start+self.cell_size] = data
+        except IOError:
+            pass
 
     def load(self, dirname):
         if not os.path.isdir(dirname):
             raise Exception('Argument must be a directory {}'.format(dirname))
 
-        print [self._process_cell(e, i) for i, e in enumerate(self.cells)]
+        self.dirname = dirname
+        files = [f for f
+                 in os.listdir(dirname)
+                 if os.path.isfile(os.path.join(dirname, f))]
+        grid = map(set, zip(*[coords(f) for f in files]))
+        self.start_cell = list(map(min, grid))
+        self.grid_size = list(map(len, grid))
+        self.matrix = numpy.empty([x * self.cell_size for x in self.grid_size])
+        self.matrix.fill(numpy.nan)
 
-        # for i in self.cells):
-            # _process_cell(self.cells(i), i)
-        # for x in range(0, cells[0]):
-        #     for y in range(0, cells[1]):
-        #         path = dirname + (fname(x, y),)
-        #         print x, y, dimension, path
-        #         x_start = x * dimension
-        #         y_start = (cells[1]-1-y) * dimension
-        #         try:
-        #             data = numpy.loadtxt(os.path.join( *path ), skiprows=6)
-        #             data[data==-9999] = numpy.nan
-        #             result[y_start:y_start+dimension,x_start:x_start+dimension] = data
-        #         except IOError:
-        #             next
+        map(self._process_cell, files)
 
+    def write_csv(self, path):
+        '''Save the current matrix as a csv file'''
+        numpy.savetxt(path, self.matrix, delimiter=",")
 
-    def savetxt(self, path):
-        pass
+    def normalise(self):
+        self.matrix[numpy.isnan(self.matrix)] = self.matrix[numpy.isfinite(self.matrix)].min() - 1
+        self.matrix -= self.matrix.min()
+        self.matrix *= 255.0/self.matrix.max()
 
-    def writepng(self, path, options={}):
-        pass
+    def write_png(self, path):
+        with open(path, 'wb') as pngfile:
+            w = png.Writer(self.cell_size*self.grid_size[0],
+                           self.cell_size*self.grid_size[1],
+                           greyscale=True)
+            w.write(pngfile, self.matrix)
 
     def summary(self):
-        pass
+        return self.matrix.min(), self.matrix.max()
--- lidarprep.py
+++ lidarprep.py
@@ -2,31 +2,20 @@
 # pylint: disable=C0103
 import sys
 import os
-# import re
 from lidar import Lidar
 
 # Last part of the path is the name
 dirname = sys.argv[1].rstrip('/')
+filename = os.path.basename(dirname).lower()
 
 if not os.path.isdir(dirname):
     raise Exception('Argument must be a directory {}'.format(dirname))
 
 # Assumes of the form LIDAR-DTM-2M-SD92
-(_, model, resolution, base) = os.path.basename(dirname).lower().split('-')
+(_, model, resolution, base) = filename.split('-')
 area = Lidar(model, resolution, base)
 area.load(dirname)
-exit()
 
-
-
-
-numpy.savetxt('output/'+filename+".csv", result, delimiter=",")
-
-pngfile = open('output/'+filename+'.png', 'wb')
-w = png.Writer(dimension*cells[0], dimension*cells[1], greyscale=True)
-result[numpy.isnan(result)] = result[numpy.isfinite(result)].min() - 1
-print result.min(), result.max()
-result -= result.min()
-result *= 255.0/result.max()
-w.write(pngfile,result)
-pngfile.close()
+area.write_csv('output/' + filename + '.csv')
+area.normalise()
+area.write_png('output/' + filename + '.png')
